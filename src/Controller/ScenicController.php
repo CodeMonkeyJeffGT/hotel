bb009a6d9bba6b2f46e6997d9a707fc6478c42c8
CondMonkeyJeffGT <18846938586@163.com>
2018-07-02 12:02:25 +0800

    debug;

--- src/Controller/ScenicController.php
+++ src/Controller/ScenicController.php
@@ -30,7 +30,8 @@ class ScenicController extends Controller
         $soap = new \SoapClient('http://47.93.39.7:8080/SOA/webservice/WebserviceTest?wsdl');
         $sId = $this->request->request->get('sId');
         $count = $this->request->request->get('count');
-        $rst = $soap->getTicket(array('sid' => $sId));
+        $param =  array('sid' => $sId);
+        $arr = $soap->__soapCall('getTicket',array('parameters' => $param));
         echo '<pre>';
         var_dump($rst);die;
         $rst = json_decode($rst->return, true);
